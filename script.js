35e8eb794907526227f9d31e5930ff1e3c67ac7d
Tech Deepak <141490582+techdeepak1996@users.noreply.github.com>
2024-04-29 18:03:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function makeid(l) {
-  // write your code here
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < l; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
 
 // Do not change the code below.
